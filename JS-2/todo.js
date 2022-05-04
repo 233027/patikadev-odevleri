c973410f26511c208820dc53a9d3df9c39fab9b3
Karam <karamguliyev10@gmail.com>
2022-05-04 23:05:22 +0300

    JS-2 Odevi | Local Storage Added

--- JS-2/todo.js
+++ JS-2/todo.js
@@ -1,29 +1,39 @@
 const newInput = document.querySelector("#task");
 const button = document.querySelector(".button");
 const list = document.querySelector("ul");
+let itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
 
+
+const data = JSON.parse(localStorage.getItem("items"));
 for (let i = 0; i < list.children.length; i++) {
   list.children[i].innerHTML += `<button style="margin-right: 20px; margin-top:15px"class="close">&times</button></li>`;
 }
-
-function newElement() {
+const liMaker = (index) => {
   if (newInput.value != "") {
-    list.innerHTML += `<li>${newInput.value}<button style = "margin-right: 20px; margin-top:15px" class="close">&times</button></li>`;
+    localStorage.setItem("items", itemsArray);
+    list.innerHTML += `<li>${newInput.value}<button  id=${index} style = "margin-right: 20px; margin-top:15px" class="close">&times</button></li>`;
     let toastBasarili = document.querySelector(".success");
     let toastAlertBasarili = new bootstrap.Toast(toastBasarili);
     toastAlertBasarili.show();
-  } else if (newInput.value == "" ) {
+    itemsArray.push(newInput.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    newInput.value = "";
+  } else if (newInput.value == "") {
     let toastBasarisiz = document.querySelector(".error");
     let toastAlertBasarisiz = new bootstrap.Toast(toastBasarisiz);
     toastAlertBasarisiz.show();
   }
-}
-
-button.addEventListener("submit", newElement);
-list.addEventListener("click", function (e) {
+};
+data.forEach((item, index) => {
+  list.innerHTML += `<li>${item}<button  id=${index} style = "margin-right: 20px; margin-top:15px" class="close">&times</button></li>`;
+});
+list.addEventListener("click", function (e, index) {
   if (e.target.className == "" || e.target.className == "checked") {
     e.target.classList.toggle("checked");
+    itemsArray.push(newInput.va);
   } else {
     e.target.parentElement.remove();
+    data.splice(e.target.id, 1);
+    localStorage.setItem("items", JSON.stringify(data));
   }
 });
